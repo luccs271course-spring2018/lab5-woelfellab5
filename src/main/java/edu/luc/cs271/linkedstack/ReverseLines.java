90af1297f22e48059919ba3704436e5a44b1ebd8
Michael Woelfel <mwoelfel@luc.edu>
2017-10-16 20:07:46 -0500

    Update ReverseLines.java

--- src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
+++ src/main/java/edu/luc/cs271/linkedstack/ReverseLines.java
@@ -1,23 +1,29 @@
 package edu.luc.cs271.linkedstack;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class ReverseLines {
 
-  public static void main(final String[] args) {
-    // TODO read successive input lines until EOF, then print out in reverse order
+  public static void main(String[] args) {
+    // Done read successive input lines until EOF, then print out in reverse order
 
-
+    LinkedStack<String> stack = new LinkedStack<String>();
+    System.out.println("Enter how many lines you want");
+    System.out.println("Press enter twice when you are finished");
 
     final Scanner input = new Scanner(System.in);
     String line;
-    while ((line = input.nextLine()) != null) {
 
-
-
+    while (input.hasNextLine()) {
+      line = input.nextLine();
+      stack.push(line);
+      if (line.equals("")) {
+        break;
+      }
     }
 
-
-
+    while (!stack.isEmpty()) {
+      System.out.println(stack.pop());
+    }
   }
 }
